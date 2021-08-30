b11482f6ca3597173c7e2d6e3ec6e0c6deb2cf5e
Hichami Houssam <hichami.houssam@gmail.com>
2021-08-30 16:54:58 +0200

    hamburger button displays menu

--- index.js
+++ index.js
@@ -1,15 +1,45 @@
+const body = document.querySelector('body');
 const hamburger = document.querySelector('.hamburger');
+const close = document.querySelector('.close');
+
+
+
 const menu = document.querySelector('.hide_mobile');
 
+
+
 hamburger.addEventListener('click', openMenu);
+close.addEventListener('click', closeMenu);
 
 
 
-function openMenu(event) {
+function openMenu() {
+
+    if(menu.classList.toggle("hamburger-toggle",true)){
+
+        
+        /*menu.classList.toggle("hamburger-toggle");*/
+        hamburger.classList.toggle("hide_hamburger"); 
+        close.classList.toggle("close-toggle");
+        body.style.overflow = "hidden";
+
+    }
+
+    /*else{
+        body.style.overflow = "visible";
+    }*/
 
 
-    menu.style.display = "block";
     
-    console.log(event.target);
+    
+}
+
+function closeMenu() {
+
+    menu.classList.remove("hamburger-toggle");
+    close.classList.remove("close-toggle");
+    hamburger.classList.toggle("hide_hamburger");
+    body.style.overflow = "visible";
+
 }
 
