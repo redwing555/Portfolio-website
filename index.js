26455e10ad5d4d632855ecaf1032cb3018447ec6
Hichami Houssam <hichami.houssam@gmail.com>
2021-08-30 20:02:00 +0200

    added responsive mobile menu with no linter errors

--- index.js
+++ index.js
@@ -2,44 +2,28 @@ const body = document.querySelector('body');
 const hamburger = document.querySelector('.hamburger');
 const close = document.querySelector('.close');
 
-
-
 const menu = document.querySelector('.hide_mobile');
 
-
+function openMenu() {
+  if (menu.classList.toggle('hamburger-toggle', true)) {
+    hamburger.classList.toggle('hide_hamburger');
+    close.classList.toggle('close-toggle');
+    body.style.overflow = 'hidden';
+  }
+}
 
 hamburger.addEventListener('click', openMenu);
-close.addEventListener('click', closeMenu);
-
-
-
-function openMenu() {
-
-    if(menu.classList.toggle("hamburger-toggle",true)){
-
-        
-       
-        hamburger.classList.toggle("hide_hamburger"); 
-        close.classList.toggle("close-toggle");
-        body.style.overflow = "hidden";
-
-    }
-
-
-    
-    
-}
 
 function closeMenu() {
-
-    menu.classList.remove("hamburger-toggle");
-    close.classList.remove("close-toggle");
-    hamburger.classList.toggle("hide_hamburger");
-    body.style.overflow = "visible";
-
+  menu.classList.remove('hamburger-toggle');
+  close.classList.remove('close-toggle');
+  hamburger.classList.toggle('hide_hamburger');
+  body.style.overflow = 'visible';
 }
 
+close.addEventListener('click', closeMenu);
+
 const navList = document.querySelectorAll('.mnitem');
-for (let i = 0; i < navList.length; i++) {
-    navList[i].addEventListener('click', closeMenu);
-} 
+for (let i = 0; i < navList.length; i += 1) {
+  navList[i].addEventListener('click', closeMenu);
+}
